b7e1b495fbf1d55d530d7eca3e9a44dbbe265c73
Byrapuneni Mahima Chowdary <89603673+mahimabyrapuneni@users.noreply.github.com>
2021-11-12 08:43:50 -0600

    Add files via upload

--- routes/resource.js
+++ routes/resource.js
@@ -28,6 +28,32 @@ router.get('/watch/:id', watch_controller.watch_detail);
  
 // GET request for list of all watch items. 
 router.get('/watch', watch_controller.watch_list); 
+// GET request for one watch. 
+router.get('/watchs/:id', watch_controller.watch_detail); 
+
+// Handle watch update form on PUT. 
+exports.watch_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await watch.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.watch_type)  
+               toUpdate.watch_type = req.body.watch_type; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    }
+    if(req.body.checkboxsale) toUpdate.sale = true; 
+else toUpdate.same = false; 
+}; 
+ 
+ 
  
 module.exports = router; 
 
